6edcbe24d843eb7d4bcdb93c604bbd55358f5634
Laimonas Danyla <laimonas.danyla@gmail.com>
2016-11-05 18:39:55 +0200

    refactor: add a priority button and its functionality

--- js/models/todo.js
+++ js/models/todo.js
@@ -13,12 +13,24 @@ var app = app || {};
 		// and ensure that each todo created has `title` and `completed` keys.
 		defaults: {
 			title: '',
-			completed: false
+			completed: false,
+			//need to add priority false as well
+			// so whne new todo item is created they will have also a priority false
+			priority: false
+		},
+
+		//toggle the 'priority' sate of this todo item.
+		togglePriority: function() {
+			this.save({
+				priority: !this.get('priority')
+			});
 		},
 
 		// Toggle the `completed` state of this todo item.
 		toggle: function () {
 			this.save({
+				//!this. means not (in this case not comleted)
+				//check how.save and this.get works in backbonejs
 				completed: !this.get('completed')
 			});
 		}
--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -18,9 +18,14 @@ var app = app || {};
 		// The DOM events specific to an item.
 		events: {
 			'click .toggle': 'toggleCompleted',
-			//add a event name for click of the edti button
-			'click .edit-btn' : 'edit',
+			//add a event name for click of the edit button
+			//that was enought to make edit button working due to
+			// backbonejs features described in View -Extend
+			//http://backbonejs.org/#View-extend
+			'click .edit-btn': 'edit',
 			'dblclick label': 'edit',
+			//connect priority button
+			'click .priority-btn': 'togglePriority',
 			'click .destroy': 'clear',
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
@@ -52,6 +57,7 @@ var app = app || {};
 
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('completed', this.model.get('completed'));
+			this.$el.toggleClass('priority', this.model.get('priority'));
 			this.toggleVisible();
 			this.$input = this.$('.edit');
 			return this;
@@ -65,6 +71,10 @@ var app = app || {};
 			return this.model.get('completed') ?
 				app.TodoFilter === 'active' :
 				app.TodoFilter === 'completed';
+		},
+		// Toggle the `"priority"` state of the model.
+		togglePriority: function () {
+			this.model.togglePriority();
 		},
 
 		// Toggle the `"completed"` state of the model.
